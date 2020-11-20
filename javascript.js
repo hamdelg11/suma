4c3c826cf39135219f9fdb01ee6c26af516dd22d
hamdelg11 <73626276+hamdelg11@users.noreply.github.com>
2020-11-19 22:34:04 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function suma(){
+
+  var n1=Number(document.getElementById('n1').value);
+  var n2=Number(document.getElementById('n2').value);
+  var re= n1+n2;
+
+ document.getElementById('re').value=re;
+
+
+
+
+}
